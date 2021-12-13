ecba57ba780aff916000ee9cb4b3532c1567dced
千帐 <qianzhang.lj@antgroup.com>
2021-12-13 19:56:44 +0800

    feat: add reg and design-pattern/event-emitter function and test case

--- src/design-pattern/event-emitter-function-version.ts
+++ src/design-pattern/event-emitter-function-version.ts
@@ -1,30 +1,49 @@
-function FunctionEventEmitter(){
-    this._events = {}
+function FunctionEventEmitter() {
+    this._events = {};
 }
 
-FunctionEventEmitter.prototype.on = function (type:string, listener:Function) {
-    this._events[type] = (this._events[type] ||[]).push(listener)
-}
-FunctionEventEmitter.prototype.emit = function (type:string, ...args:any[]) {
-    if(!this._events[type] || this._events[type].length === 0)
-        return
-    this._events[type].forEach((listener:Function) => listener.call(this, ...args))
-}
+FunctionEventEmitter.prototype.on = function (
+    type: string,
+    listener: Function
+) {
+    if (this._events[type]) {
+        this._events[type].push(listener);
+    } else {
+        this._events[type] = [listener];
+    }
+};
+FunctionEventEmitter.prototype.emit = function (type: string, ...args: any[]) {
+    if (!this._events[type] || this._events[type].length === 0) return;
+
+    this._events[type].forEach((listener: Function) =>
+        listener.call(this, ...args)
+    );
+};
 /**
  * 难点实现
  * @param type
  * @param listener
  */
-FunctionEventEmitter.prototype.once = function (type:string, listener:Function) {
-    const fn = function (){
-    }
+FunctionEventEmitter.prototype.once = function (
+    type: string,
+    listener: Function
+) {
+    let foo = function (...args) {
+        // 执行完注销，而不是注册完注销
+        listener.call(this, ...args);
+        this.off(type, foo);
+    };
+    (foo as any).link = listener;
+    this.on(type, foo);
+};
 
-    this._events[type] = (this._events[type] ||[]).push(fn)
-}
-FunctionEventEmitter.prototype.off = function (type:string, listener:Function) {
-    this._events[type] = this._events[type].filter((func:Function)=>{
-        return func !==listener
-    })
-}
+FunctionEventEmitter.prototype.off = function (
+    type: string,
+    listener: Function
+) {
+    this._events[type] = this._events[type].filter((func: any) => {
+        return func !== listener && func.link !== listener;
+    });
+};
 
-export default FunctionEventEmitter
+export default FunctionEventEmitter;
--- test/design-pattern/event-emitter-function-version.spec.ts
+++ test/design-pattern/event-emitter-function-version.spec.ts
@@ -1,8 +1,38 @@
-// function fakeHandler() {
-//     console.log('called')
-//
-// }
-//
-// test('should called',()=>{
-//     ev.on('event_001', fakeHandler)
-// })
+import eventEmitterFunctionVersion from "../../src/design-pattern/event-emitter-function-version";
+
+const EVENT_NAME = "event_001";
+
+let ev;
+let fakeFn;
+
+beforeEach(() => {
+    ev = new eventEmitterFunctionVersion();
+    fakeFn = jest.fn();
+});
+
+test("should emit & on", () => {
+    ev.on(EVENT_NAME, fakeFn);
+    ev.emit(EVENT_NAME, 100, "hello,world");
+    expect(fakeFn).toHaveBeenCalledTimes(1);
+});
+
+test("should off", () => {
+    ev.on(EVENT_NAME, fakeFn);
+    ev.off(EVENT_NAME, fakeFn);
+    ev.emit(EVENT_NAME, 100, "hello,world");
+    expect(fakeFn).not.toHaveBeenCalled();
+});
+
+test("should emit once", () => {
+    ev.once(EVENT_NAME, fakeFn);
+    ev.emit(EVENT_NAME);
+    ev.emit(EVENT_NAME);
+    expect(fakeFn).toHaveBeenCalledTimes(1);
+});
+
+test("should emit nothing if off is called", () => {
+    ev.once(EVENT_NAME, fakeFn);
+    ev.off(EVENT_NAME, fakeFn);
+    ev.emit(EVENT_NAME);
+    expect(fakeFn).not.toHaveBeenCalled();
+});
