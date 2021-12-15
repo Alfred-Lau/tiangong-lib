6991bf402556ee4f3dea1781d881877c423df083
Alfred-Lau <jian.liu@icloud.com>
2021-12-15 17:52:54 +0800

    feat: add some type function singature

---
+++ src/type-gym/index.ts
@@ -0,0 +1,17 @@
+export type TPartial<T> = {
+  [P in keyof T]?: T[P];
+};
+
+export type TRequired<T> = {
+  [P in keyof T]-?: T[P];
+};
+export type TPick<T, K extends keyof T> = {
+  [P in K]: T[P];
+};
+export type TExclude<T, U> = T extends U ? never : T;
+
+export type TOmit<T, K extends keyof any> = Pick<T, Exclude<keyof T, K>>;
+
+export type TRecord<K extends keyof any, T> = {
+  [P in K]: T;
+};
