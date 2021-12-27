1d550b84a099c9625d4b0fb27cfc06d3759201f5
Alfred-Lau <jian.liu@icloud.com>
2021-12-28 00:52:56 +0800

    feat: add 1-20 handy functions

--- src/util/cancelablePromsie.ts
+++ src/util/cancelablePromsie.ts
@@ -1,4 +1,37 @@
 /**
  * 具备取消功能的promise函数
  */
-export default class CancelablePromise {}
+export default function cancelPromise(
+  promise: Promise<any>
+): Promise<any> & { cancel: ((reason: string) => void) | undefined } {
+  let cancel: ((reason: string) => void) | undefined = undefined;
+  let isCanceled = false;
+
+  const cancelPromise = new Promise<any>((resolve, reject) => {
+    cancel = (reason: string) => {
+      isCanceled = true;
+      reject(reason);
+    };
+  });
+
+  const racePromise = Promise.race([promise, cancelPromise]).catch((err) => {
+    if (isCanceled) {
+      return new Promise(() => {});
+    }
+
+    return Promise.reject(err);
+  });
+
+  return Object.assign(racePromise, { cancel });
+}
+
+const p = new Promise((resolve) => {
+  setTimeout(() => {
+    console.log("print");
+    resolve("");
+  }, 2000);
+});
+
+const cp = cancelPromise(p);
+
+cp.cancel!("");
