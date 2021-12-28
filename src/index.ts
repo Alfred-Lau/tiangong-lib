66dda2f5d040f6c68eae018e9ce35a64ef15dfe9
Alfred-Lau <jian.liu@icloud.com>
2021-12-29 02:41:59 +0800

    fix: add semantic-release

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 export * as dp from "./design-pattern";
 export * as device from "./device";
-export * as dom from "./type";
+export * as dom from "./dom";
 export * as handy from "./handy-functions";
 export * as hooks from "./hook";
 export * as lodash from "./lodash";
 export * as media from "./media";
 export * as moment from "./moment";
 export * as reg from "./reg";
-export * as type from "./type";
+export * as types from "./type";
 export * as typeGym from "./type-gym";
 export * as util from "./util";
