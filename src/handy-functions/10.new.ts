9bce91342633af7f060abb83ba0660195ef6c6f4
Alfred-Lau <jian.liu@icloud.com>
2021-12-28 22:44:45 +0800

    feat: add 10.new

--- src/handy-functions/10.new.ts
+++ src/handy-functions/10.new.ts
@@ -1,1 +1,51 @@
-export default function New() {}
+/*
+让实例可以访问到私有属性
+让实例可以访问构造函数原型(constructor.prototype)所在原型链上的属性
+如果构造函数返回的结果不是引用数据类型
+
+
+实现 new 操作符
+
+new操作符做了这些事：VIP !!!!
+
+- 它创建了一个全新的对象。
+- 它会被执行[[Prototype]]（也就是__proto__）链接。
+- 它使this指向新创建的对象。。
+- 通过new创建的每个对象将最终被[[Prototype]]链接到这个函数的prototype对象上。
+- 如果函数没有返回对象类型Object(包含Function, Array, Date, RegExg, Error)，那么new表达式中的函数调用将返回该对象引用。
+
+*/
+
+export default function New(ctx) {
+  //1. 创建一个全新的对象
+  const res: { __proto__?: unknown } = {};
+
+  //2. 绑定原型链链接
+  if (ctx.prototype !== null) {
+    // !!!!对象的隐式原型指向构造函数的显式原型，因为不能使用 new，所以必须自己构造解决
+    res.__proto__ = ctx.prototype;
+  }
+
+  //3. 执行构造函数
+  let ret = ctx.apply(res, Array.prototype.slice.call(arguments, 1));
+
+  if ((typeof ret === "object" || typeof ret === "function") && ret !== null) {
+    /* 4. 如果构造函数有返回值，并且不为 null， 那么就使用 返回值 */
+    return ret;
+  }
+
+  return res;
+}
+
+/*
+为什么不能直接使用 slice 必须通过原型链上面的方便call ？
+
+！！！因为 arguments 等类似数组对象并没有 数组方法！！！！！！
+
+【类数组对象转换为数组的方式】
+
+1. [...arguments]
+2. Array.from(arguments)
+3. [].slice.call(arguments)
+
+*/
