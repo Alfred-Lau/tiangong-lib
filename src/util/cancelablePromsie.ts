d794979eea8645b88ca161cdc4e191e2b62468b2
Alfred-Lau <jian.liu@icloud.com>
2022-01-26 21:55:43 +0800

    fix: add cancelablePromsie

--- src/util/cancelablePromsie.ts
+++ src/util/cancelablePromsie.ts
@@ -28,10 +28,42 @@ export default function cancelPromise(
 const p = new Promise((resolve) => {
   setTimeout(() => {
     console.log("print");
-    resolve("");
-  }, 2000);
+    resolve("我是正常的返回");
+  }, 4000);
 });
 
 const cp = cancelPromise(p);
+cp.then((res) => console.log(res)).catch((err) => console.log(err));
+setTimeout(() => cp.cancel!("cccccc"), 2000);
 
-cp.cancel!("");
+/**
+ * 具备 cancel 功能的简易实现: 参考文件：https://juejin.cn/post/6847902216028848141
+ */
+//
+// function abortWrapper(original) {
+//   let abort;
+//
+//   let p1 = new Promise((resolve, reject) => {
+//     return (abort = reject);
+//   });
+//
+//   let p = Promise.race([p1, original]) as Promise<any> & {
+//     abort: (msg: string) => void;
+//   };
+//   p.abort = abort;
+//
+//   return p;
+// }
+//
+// const request = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("收到服务端数据");
+//   }, 3000);
+// });
+//
+// const req = abortWrapper(request);
+// req
+//   .then((res) => console.log("resp", res))
+//   .catch((e) => console.log("error", e));
+// setTimeout(() => req.abort("用户手动终止请求"), 2000); // 这里可以是用户主动点击
+// setTimeout(() => req.abort("用户手动终止请求"), 2000); // 这里可以是用户主动点击
