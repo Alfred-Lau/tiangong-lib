e81cdf11fbbd03cbe95aec2641e25167d19c99b1
Alfred-Lau <jian.liu@icloud.com>
2022-01-27 20:08:42 +0800

    fix: add promise

--- src/handy-functions/7.promiseFunctionType.ts
+++ src/handy-functions/7.promiseFunctionType.ts
@@ -2,8 +2,27 @@ const PENDING = Symbol("Promise#PENDING");
 const FULFILLED = Symbol("Promise#FULFILLED");
 const REJECTED = Symbol("Promise#REJECTED");
 
+/**
+ * 需要依次解决的问题
+ * 1. 结构初始化
+ * 2. 支持简单的 then
+ *
+ * @param fn
+ */
+
 function promise(fn) {
-  // this.
+  const resolve = function (value) {
+    console.log(value);
+  };
+  const reject = function (reason) {
+    console.log(reason);
+  };
+  fn(resolve, reject);
 }
 
 export default promise;
+
+const p1 = new promise((resolve, reject) => {
+  resolve({ name: "fenghuo" });
+  reject("error");
+});
