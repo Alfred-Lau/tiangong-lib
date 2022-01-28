fcf9bea972390db096dbc0f5dba71a591a3491d4
Alfred-Lau <jian.liu@icloud.com>
2022-01-28 12:45:55 +0800

    fix: add general type judge

--- src/type/index.ts
+++ src/type/index.ts
@@ -34,6 +34,35 @@ export function isFalsy(val: any): boolean {
  * 判断 变量是否为函数
  * @param obj
  */
-export function isFunction(obj: any): obj is Function {
+export function isFunction_v2(obj: any): obj is Function {
   return typeof obj === "function";
 }
+
+/**
+ * 通用判断体系：https://www.cnblogs.com/bq-med/p/8796836.html
+ * 深入解读：https://zhuanlan.zhihu.com/p/118793721
+ */
+
+/**
+ * 若参数为 null 或 undefined，直接返回结果。
+ * 若参数不为 null 或 undefined，则将参数转为对象，再作判断。
+ * 对于原始类型，转为对象的方法即装箱，此处不赘述。
+ * 转为对象后，取得该对象的 [Symbol.toStringTag] 属性值（可能会遍历原型链）作为 tag，
+ * 如无该属性，或该属性值不为字符串类型，则依下表取得 tag, 然后返回 "[object " + tag + "]" 形式的字符串。
+ */
+
+//
+const toString = Object.prototype.toString;
+
+// .call 或者 Reflect.apply(Object.prototype.toString, x, []);
+export const isNumber = (value) => toString.call(value) === "[object Number]";
+export const isString = (value) => toString.call(value) === "[object String]";
+export const isUndefined = (value) =>
+  toString.call(value) === "[object Undefined]";
+export const isBoolean = (value) => toString.call(value) === "[object Boolean]";
+export const isObject = (value) => toString.call(value) === "[object Object]";
+export const isArray = (value) => toString.call(value) === "[object Array]";
+// 区别于 Object.prototype.toString(a) 和 isFunction_v2
+export const isFunction = (value) =>
+  toString.call(value) === "[object Function]";
+export const isNull = (value) => toString.call(value) === "[object Null]";
